150d9568550f827b93a564f21dda0796f30a735f
hayaabdallah <100359839+hayaabdallah@users.noreply.github.com>
2022-07-23 09:26:37 +0100

    main

--- carrental/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="footer">
-  <div class="container">
-    <div class="copyright">
-      &copy; Copyright <strong>Carrental portal</strong>. All Rights Reserved
-    </div>
-  </div>
-</footer>
